bb0f0fceeca5837ea3fb570496b5b983277838e9
kmats <the_big0@hotmail.com>
2013-12-26 16:42:51 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Daicho::Application.config.secret_token = 'fb45517fdd24e46b8dc93bde4d6fef3ba91fea4db6275d222d4fe4913f6de1287f86ab8c0e0d8532e2116dd333bfbacfd752206f7a3f9b048048c2116ffa3542'
